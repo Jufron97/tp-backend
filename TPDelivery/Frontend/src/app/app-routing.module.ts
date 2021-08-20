6f65c9e444a4dfa23f771ef753530750b6970e26
Gonza077 <gonzalo07.xc@gmail.com>
2021-08-20 11:55:57 -0300

    Eliminacion de carpetas sin uso.

--- TPDelivery/Frontend/src/app/app-routing.module.ts
+++ TPDelivery/Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearLocalComponent } from './components/crear-local/crear-local.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListLocalComponent } from './components/list-local/list-local.component';
+import { ListProductosComponent } from './components/list-productos/list-productos.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LocalesComponent } from './components/locales/locales.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:LandingComponent},
+  { path: 'crear-local', component: CrearLocalComponent},
+  { path: 'listar-producto/:idL', component: ListProductosComponent},
+  { path: 'crear-producto/:idL', component: CrearProductoComponent},
+  { path: 'editar-producto/:idL/:idP', component: CrearProductoComponent},
+  { path:'locales', component:LocalesComponent},
+  { path:'productos', component:ProductosComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- TPDelivery/Frontend/src/app/app.module.ts
+++ TPDelivery/Frontend/src/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmCoreModule } from '@agm/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { CrearLocalComponent } from './components/crear-local/crear-local.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListProductosComponent } from './components/list-productos/list-productos.component';
+import { ListLocalComponent } from './components/list-local/list-local.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LocalesComponent } from './components/locales/locales.component';
+import { MapComponent } from './components/map/map.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    CrearLocalComponent,
+    CrearProductoComponent,
+    ListProductosComponent,
+    ListLocalComponent,
+    LandingComponent,
+    NavbarComponent,
+    LocalesComponent,
+    MapComponent,
+    ProductosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule,
+    ToastrModule.forRoot(),
+    HttpClientModule,
+    NgbModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBOOnK5VbqwfeYaM2zWDOFxG1XzFNrbMm4'
+    }),
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
