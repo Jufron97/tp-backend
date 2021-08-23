d8010516e132eb9c9947bb7b0456773d7b5a8ddd
Gonza077 <gonzalo07.xc@gmail.com>
2021-08-23 20:35:37 -0300

    Incorporacion de declaraciones y imports para Test

--- TPDelivery/Frontend/src/app/app.component.spec.ts
+++ TPDelivery/Frontend/src/app/app.component.spec.ts
@@ -1,15 +1,58 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmCoreModule } from '@agm/core';
+
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearLocalComponent } from './components/crear-local/crear-local.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListProductosComponent } from './components/list-productos/list-productos.component';
+import { ListLocalComponent } from './components/list-local/list-local.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LocalesComponent } from './components/locales/locales.component';
+import { MapComponent } from './components/map/map.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { AbmLocalComponent } from './components/abm-local/abm-local.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        ToastrModule,
+        ToastrModule.forRoot(),
+        HttpClientModule,
+        NgbModule,
+        AgmCoreModule.forRoot({
+          apiKey: 'AIzaSyBOOnK5VbqwfeYaM2zWDOFxG1XzFNrbMm4'
+        })        
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CrearLocalComponent,
+        CrearProductoComponent,
+        ListProductosComponent,
+        ListLocalComponent,
+        LandingComponent,
+        NavbarComponent,
+        LocalesComponent,
+        MapComponent,
+        ProductosComponent,
+        AbmLocalComponent,
+        FooterComponent,
       ],
     }).compileComponents();
   });
