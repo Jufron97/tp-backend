41a5f0e832741cacdd5f22bb9740e7451256d82a
Juan Frontons <jfrontons@frro.utn.edu.ar>
2021-08-24 20:07:09 -0300

    nuevos nombres de carpetas

--- TPDelivery/Backend/src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// conectamos db
-mongoose.connect('mongodb://localhost/locales',{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  })
-  .then(db => console.log('db connected'))
-  .catch(err => console.log(err));
